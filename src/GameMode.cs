461e542d921338a843f275d7c1e6f3bc7ff2e552
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-07-01 16:20:11 +0200

    Added project files

---
+++ src/GameMode.cs
@@ -0,0 +1,21 @@
+﻿namespace Hardstuck.GuildWars2
+{
+    /// <summary>
+    /// Game modes
+    /// </summary>
+    public enum GameMode
+    {
+        /// <summary>
+        /// Player vs Enviroment
+        /// </summary>
+        PvE,
+        /// <summary>
+        /// Structured Player vs Player
+        /// </summary>
+        PvP,
+        /// <summary>
+        /// World vs World
+        /// </summary>
+        WvW
+    }
+}
---
+++ src/Race.cs
@@ -0,0 +1,29 @@
+﻿namespace Hardstuck.GuildWars2
+{
+    /// <summary>
+    /// Character race
+    /// </summary>
+    public enum Race : uint
+    {
+        /// <summary>
+        /// Asura
+        /// </summary>
+        Asura = 0,
+        /// <summary>
+        /// Charr
+        /// </summary>
+        Charr = 1,
+        /// <summary>
+        /// Human
+        /// </summary>
+        Human = 2,
+        /// <summary>
+        /// Norn
+        /// </summary>
+        Norn = 3,
+        /// <summary>
+        /// Sylvari
+        /// </summary>
+        Sylvari = 4
+    }
+}
