3b4f66346a3b4a2e76ecba9c1eeb40e5e249a045
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-08-31 09:55:53 +0200

    Add Utils.cs for handling character name capitalisation

---
+++ src/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hardstuck.GuildWars2
+{
+    /// <summary>
+    /// Provides sets of utility methods for handling character names.
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Converts the string to a properly spaced character name.
+        /// </summary>
+        /// <param name="characterName">unconverted character name</param>
+        /// <returns>converted character name</returns>
+        public static string ToCharacterName(this string characterName)
+        {
+            return ConvertToCharacterName(characterName);
+        }
+
+        /// <summary>
+        /// Converts the string to a properly spaced character name.
+        /// </summary>
+        /// <param name="characterName">unconverted character name</param>
+        /// <returns>converted character name</returns>
+        public static string ConvertToCharacterName(string characterName)
+        {
+            List<string> nameParts = characterName.Split(' ').ToList();
+            return nameParts.Select(x => x.ToLower()).Select(x => $"{x[0].ToString().ToUpper()}{x.Substring(1)}").Aggregate((x, y) => $"{x} {y}");
+        }
+    }
+}
