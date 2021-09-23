83c64cbeca5958014d53c879fe2a082e572a2591
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-09-23 11:24:54 +0200

    Added GameRenderMode enum

---
+++ src/GameRenderMode.cs
@@ -0,0 +1,18 @@
+﻿namespace Hardstuck.GuildWars2
+{
+    /// <summary>
+    /// Render mode for the game (DirectX 9 or DirectX 11)
+    /// </summary>
+    public enum GameRenderMode
+    {
+        /// <summary>
+        /// DirectX 9
+        /// </summary>
+        DX9 = 0,
+
+        /// <summary>
+        /// DirectX 11
+        /// </summary>
+        DX11 = 1
+    }
+}
